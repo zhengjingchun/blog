e6a0a517613fefafb720972d542c429be0a02512
zhengjingchun <877182978@qq.com>
2018-03-15 22:17:56 +0800

    404页面

--- app.js
+++ app.js
@@ -51,9 +51,10 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    res.render("404");
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  // next(err);
 });
 
 // error handler
